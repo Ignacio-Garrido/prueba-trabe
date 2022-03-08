505c6c2449c655eb4445ae64b1f983c8e5e7ca32
Ignacio <ignacio.garrido@udc.es>
2022-03-08 16:05:50 +0100

    Spring Initializr

---
+++ src/main/java/com/prueba/trabe/TrabeApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.trabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/trabe/TrabeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.trabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
